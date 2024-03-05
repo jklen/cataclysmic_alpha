6a323aea02ad5fcffbcf4da4c66bc486663072eb
Jaroslav Klen <klen.jaro@gmail.com>
2024-03-05 23:05:29 +0100

    run portfolio in progress

--- scripts/run_portfolio.py
+++ scripts/run_portfolio.py
@@ -8,7 +8,7 @@ import yaml
 from datetime import datetime
 from utils_strategy import data_load
 from utils_portfolio import check_weights, run_strategy, check_today_signals, \
-    orders_or_close, review_positions
+    orders_or_close, review_positions, strategies_directions
     
 with open('../portfolio_logging_config.json', 'r') as config_file:
     config_dict = json.load(config_file)
@@ -23,38 +23,31 @@ def main(path_config):
     config = yaml.safe_load(open(path_config, 'r'))
     
     for portfolio in config.keys():
-        review_positions() # check v alpace ci nebol trigernuty stop loss alebo take profit
-        weights = check_weights(config[portfolio])
+        weights = check_weights(config[portfolio]['symbols'].keys(), config[portfolio]['weights'])
         for symbol in config[portfolio]['symbols']:
+            review_positions(symbol)
             #pdb.set_trace()
             df_symbol = data_load(symbol, 
                                   config[portfolio]['data_preference'],
                                   datetime(2000, 1, 1), 
-                                  datetime(2024, 2, 29))
+                                  datetime.today().date())
             print(df_symbol.tail(1))
-            for strategy in config[portfolio]['symbols'][symbol].keys():
-                entries, exits = run_strategy(portfolio, 
-                                              df_symbol, 
-                                              symbol, 
-                                              strategy, 
-                                              strategy_params)
-                today_entry, today_exit = check_today_signals(entries, exits)
-                orders_or_close(weights, 
-                                portfolio, 
-                                symbol, 
-                                strategy, 
-                                today_entry.loc[today_entry == True],
-                                today_exit.loc[today_exit == True])
+            strategy = config[portfolio][symbol].keys()[0]
+            strategy_params = config[portfolio][symbol][strategy]
+            strategy_direction = strategies_directions[strategy]
+            entries, exits = run_strategy(df_symbol, 
+                                          symbol, 
+                                          strategy, 
+                                          strategy_params) #TODO
+            today_entry, today_exit = check_today_signals(entries, exits) #true/false
+            orders_or_close(weights[symbol], 
+                            symbol, 
+                            today_entry,
+                            today_exit,
+                            strategy_direction)
             
     
 if __name__ == '__main__':
     main()
     
-    
-#   1. spustim script, ktory natiahne najnovsie data z alpacy alebo yf, pre kazdy symbol
-#   2. pre kazdu strategiu a kazdu parameter kombinaciu chekni ci je dnes entry alebo exit signal
-#   3. vyhodnot vahy (bude ako 1. po prvej iteracii)
-#   4. ak entry signal, daj order. Ked bude order filled, id pozicie prirad ku konkretnemu portfoliu
-#   5. ak exit signal, closni poziciu pre dany symbol, strategiu a param kombinaciu pod danym postion id
-# 
-# ak market order + stop order - trigernutie stop loss sa moze bit s vectorbt signalmi
+# ak market order + stop order - trigernutie stop loss sa moze bit s vectorbt signalmi kvoli close priceyg
---
+++ scripts/utils_portfolio.py
@@ -0,0 +1,19 @@
+
+strategies_directions = {
+    'hhhl':'long'
+}
+
+def review_positions():
+    pass
+
+def check_weights():
+    pass
+
+def run_strategy():
+    pass
+
+def check_today_signals():
+    pass
+
+def orders_or_close():
+    pass

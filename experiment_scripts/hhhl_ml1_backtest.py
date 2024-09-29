d9e5911b9ee0515c12175e45919506ae3e633f05
Jaroslav Klen <klen.jaro@gmail.com>
2024-09-29 23:56:26 +0200

    hhhl ml strategy fix

--- experiment_scripts/hhhl_ml1_backtest.py
+++ experiment_scripts/hhhl_ml1_backtest.py
@@ -6,9 +6,10 @@ import sys
 import os
 import pickle
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from scripts.strategies import hhhl_ml1_strategy_logic, HigherHighStrategyML1
+from scripts.strategies import hhhl_ml1_strategy_logic
 import pdb
 import vectorbt as vbt
+import plotly.io as pio
 
 #%% load data
 df_price = pd.read_csv('../data/df_price_aapl.csv', index_col='Date', parse_dates = ['Date'])
@@ -27,51 +28,84 @@ params = {'window_entry': [2, 9],
       'lh_counts': [1, 5]}
 
 #%% backtest loop
-
 stats_all = []
 for i in range(0, len(train_stuff['train_mins'])):
-    
-    df_true_entry_signals_train, df_true_exit_signals_train = hhhl_ml1_strategy_logic([datasets_train[i]],
-                                                    [train_stuff['gridsearch_objs'][i]],
+    train_result = hhhl_ml1_strategy_logic(datasets_train[i],
+                                                    train_stuff['gridsearch_objs'][i],
                                                     params,
                                                     0.5)
-    df_true_entry_signals_test, df_true_exit_signals_test = hhhl_ml1_strategy_logic([datasets_test[i]],
-                                                        [train_stuff['gridsearch_objs'][i]],
+    true_entry_signals_train = train_result[0]
+    true_exit_signals_train = train_result[1]
+    max_combs_all_train = train_result[2]
+    max_probs_all_train = train_result[3]
+    
+    test_result = hhhl_ml1_strategy_logic(datasets_test[i],
+                                                        train_stuff['gridsearch_objs'][i],
                                                         params,
                                                         0.5)
+    true_entry_signals_test = test_result[0]
+    true_exit_signals_test = test_result[1]
+    max_combs_all_test = test_result[2]
+    max_probs_all_test = test_result[3]
 
     pf_train = vbt.Portfolio.from_signals(datasets_train[i]['Adj Close'], 
-                                    df_true_entry_signals_train, 
-                                    df_true_exit_signals_train,
+                                    true_entry_signals_train, 
+                                    true_exit_signals_train,
                                     price = datasets_train[i]['Open'].shift(-1),
-                                    fees=0.001, 
-                                    slippage=0.0001,
+                                    #fees=0.001, 
+                                    #slippage=0.0001,
                                     direction = 'longonly', 
-                                    freq='1D')
+                                    freq='1D',
+                                    sl_stop=0.1)
     stats_train = pf_train.stats(agg_func = None).T
     stats_train.columns = [f"{i}_train"]
 
     pf_test = vbt.Portfolio.from_signals(datasets_test[i]['Adj Close'], 
-                                    df_true_entry_signals_test, 
-                                    df_true_exit_signals_test,
+                                    true_entry_signals_test, 
+                                    true_exit_signals_test,
                                     price = datasets_test[i]['Open'].shift(-1),
-                                    fees=0.001, 
-                                    slippage=0.0001,
+                                    #fees=0.001, 
+                                    #slippage=0.0001,
                                     direction = 'longonly', 
-                                    freq='1D')
+                                    freq='1D',
+                                    sl_stop = 0.1)
     stats_test  = pf_test.stats(agg_func = None).T
     stats_test.columns = [f"{i}_test"]
     
     stats_all.append(stats_train)
     stats_all.append(stats_test)
+    
+    df_info_train = pd.concat([true_entry_signals_train, true_exit_signals_train,
+                               max_combs_all_train, max_probs_all_train])
+    df_info_test = pd.concat([true_entry_signals_test, true_exit_signals_test,
+                               max_combs_all_test, max_probs_all_test])
+    
+    pf_train.trades.records_readable.to_csv(f"debug/train_{i}_trades.csv", index = True, header = True)
+    df_info_train.to_csv(f"debug/df_info_train_{i}.csv", index = True, header = True)
+    pf_test.trades.records_readable.to_csv(f"debug/test_{i}_trades.csv", index = True, header = True)
+    df_info_test.to_csv(f"debug/df_info_test_{i}.csv", index = True, header = True)
+ 
+    fig = pf_train.plot()
+    fig.update_layout(title=f"train_{i}_stats")
+    pio.write_html(fig, file = f"debug/train_{i}_stats.html")
+    fig.write_image(f"debug/train_{i}_stats.png")
+    
+    fig = pf_test.plot()
+    fig.update_layout(title=f"test_{i}_stats")
+    pio.write_html(fig, file = f"debug/test_{i}_stats.html")
+    fig.write_image(f"debug/test_{i}_stats.png")
+    
 df_stats = pd.concat(stats_all, axis = 1)
+#pdb.set_trace()
 df_stats.to_csv('df_stats_aapl.csv', index = True, header = True)
 
 #TODO
 # spravna metrika na modely
 # liftcharty
 # probability threshold ako param (ked zvysim, zvysi sa mi win rate strategie?)
+# custom scoring metrika, eg. meria precision na top n podla predikovanej probability
 # entry exit signaly obchodu musia byt rovnake ako v orig. strategii !!!
+# koreluje nejaka metrika modelu s nejakou metrikou strategie?
 # zanalyzovat obchody na train a test periodach strategie:
 #   uspesny obchod vs. probability, liftcharty
 #   trvanie obchodu vs. win rate
--- scripts/strategies.py
+++ scripts/strategies.py
@@ -39,86 +39,82 @@ HigherHighStrategy = vbt.IndicatorFactory(
 
 # HigherHigh ML strategy 1
 
-def hhhl_ml1_strategy_logic(datasets:list, models:list, dataset_params:dict, 
+def hhhl_ml1_strategy_logic(dataset, model, dataset_params:dict, 
                            probability_threshold:float):
-    true_entry_signals = []
-    true_exit_signals = []
+    
+    close = dataset['Adj Close']
+    expanded_params = {key: list(range(val[0], val[1] + 1)) for key, val in dataset_params.items()}
+    combinations = list(itertools.product(*expanded_params.values()))
+    entry_signals_list = []
+    exit_signals_list = []
+    probs_list = []
+    print(f"starting processing param combinations")
+    for comb in combinations:
+        
+        # entry signals - original strategy
+        
+        higher_highs = close > close.rolling(window=comb[0], 
+                                            min_periods=comb[0]).max().shift(1)
+        higher_lows = close > close.rolling(window=comb[0], 
+                                            min_periods=comb[0]).min().shift(1)
+
+        hh_count = higher_highs.rolling(window=comb[0]).sum() # asi -1, lebo prenasa True z higher_highs z predosleho okna
+        hl_count = higher_lows.rolling(window=comb[0]).sum() # asi -1, lebo prenasa True z higher_lows z predosleho okna
+
+        entry_signal = (hl_count.shift(1) >= comb[1]) & \
+                (hh_count.shift(1) >= comb[1]) & higher_lows
+        entry_signal.name = comb
+        entry_signals_list.append(entry_signal)
+        
+        # probability predictions
+        
+        x = dataset.copy()
+        x['window_entry'] = comb[0]
+        x['hh_hl_counts'] = comb[1]
+        x['window_exit'] = comb[2]
+        x['lh_counts'] = comb[3] # dataset ready
+        predicted_proba = model.predict_proba(x)[:,1] # len True classa
+        probs_list.append(pd.Series(predicted_proba, name = comb, index = close.index))
+        
+        # exit signals - original strategy
+                            
+        lower_highs = close < close.rolling(window=comb[2], min_periods=1).max().shift(1)
+        lh_count = lower_highs.rolling(window=comb[2]).sum()
+        exit_signal = (lh_count >= comb[3]) & lower_highs
+        exit_signal.name = comb
+        exit_signals_list.append(exit_signal)
+    
+    print("param combinations finished")
+    
+    df_probs = pd.concat(probs_list, axis = 1) # df s probabilities pre kazdu param comb, eg (6,1,2,1) ako colname
+    df_entry_signals = pd.concat(entry_signals_list, axis = 1) # df s entry signalmi
+    df_exit_signals = pd.concat(exit_signals_list, axis = 1) # df s exit signalmi
+
+    df_probs_masked = df_probs.where(df_entry_signals)
+    max_combs = df_probs_masked.idxmax(axis=1) # series s param kombinaciami kde bolo max prob
+    max_probs = df_probs_masked.max(axis=1)# series s max prob
+    true_entry_signal = max_probs >= probability_threshold
+    print("calculating final entry and exit signals")
     #pdb.set_trace()
-    for i, dataset in enumerate(datasets):
-        print(f"starting processing dataset - {i}")
-        model = models[i]
-        close = dataset['Adj Close']
-        expanded_params = {key: list(range(val[0], val[1] + 1)) for key, val in dataset_params.items()}
-        combinations = list(itertools.product(*expanded_params.values()))
-        #combinations_as_dicts = [dict(zip(expanded_params.keys(), combo)) for combo in combinations]
-        df_entry_signals = pd.DataFrame(index = close.index)
-        df_exit_signals = pd.DataFrame(index = close.index)
-        df_probs = pd.DataFrame(index = close.index)
-        print(f"starting processing param combinations")
-        for comb in combinations:
-            
-            # entry signals - original strategy
-            
-            higher_highs = close > close.rolling(window=comb[0], 
-                                                min_periods=comb[0]).max().shift(1)
-            higher_lows = close > close.rolling(window=comb[0], 
-                                                min_periods=comb[0]).min().shift(1)
-
-            hh_count = higher_highs.rolling(window=comb[0]).sum() # asi -1, lebo prenasa True z higher_highs z predosleho okna
-            hl_count = higher_lows.rolling(window=comb[0]).sum() # asi -1, lebo prenasa True z higher_lows z predosleho okna
-
-            entry_signal = (hl_count.shift(1) >= comb[1]) & \
-                    (hh_count.shift(1) >= comb[1]) & higher_lows
-            df_entry_signals[comb] = entry_signal # df entry signalov, s nazvami stlpcov ako napr. (6,1,2,1)
-            
-            # probability predictions
-            
-            x = dataset.copy()
-            x['window_entry'] = comb[0]
-            x['hh_hl_counts'] = comb[1]
-            x['window_exit'] = comb[2]
-            x['lh_counts'] = comb[3] # dataset ready
-            predicted_proba = model.predict_proba(x)[:,1] # len True classa
-            df_probs[comb] = predicted_proba # df predikovanych probabilit
-            
-            # exit signals - original strategy
-                                
-            lower_highs = close < close.rolling(window=comb[2], min_periods=1).max().shift(1)
-            lh_count = lower_highs.rolling(window=comb[2]).sum()
-            exit_signal = (lh_count >= comb[3]) & lower_highs
-            df_exit_signals[comb] = exit_signal # df exit signalov
-        print("param combinations finished")    
-        df_probs_masked = df_probs.where(df_entry_signals)
-        max_combs = df_probs_masked.idxmax(axis=1) # series s param kombinaciami kde bolo max prob
-        max_probs = df_probs_masked.max(axis=1)# series s max prob
-        true_entry_signal = max_probs >= probability_threshold
-        print("calculating final entry and exit signals")
-        open_trade = False
-        for i, entry in enumerate(true_entry_signal.tolist()):
-            if open_trade:
-                exit_comb = df_exit_signals[params]
-                is_exit_signal = exit_comb[i]
-                if is_exit_signal:
-                    true_entry_signal[i] = False
-                    open_trade = False
-                else:
-                    true_entry_signal[i] = True
+    open_trade = False
+    for i, entry in enumerate(true_entry_signal.tolist()):
+        if open_trade:
+            is_exit_signal = df_exit_signals[params][i]
+            if is_exit_signal: # when closing trade, only exit signal must be True
+                true_entry_signal[i] = False
+                open_trade = False
             else:
-                if entry:
+                true_entry_signal[i] = True
+        else:
+            if entry: # when opening trade, entry signal must be True and exit signal must be False (default vbt setting)
+                params = max_combs[i]
+                if df_exit_signals[params][i] == False:
                     open_trade = True
-                    params = max_combs[i]
-        true_exit_signal = ~true_entry_signal
-        true_exit_signals.append(true_exit_signal)
-        true_entry_signals.append(true_entry_signal)
-    print("concatenating entry and exit signals for all datasets")
-    df_true_entry_signals = pd.concat(true_entry_signals, axis = 1)
-    df_true_exit_signals = pd.concat(true_exit_signals, axis = 1)
+    true_exit_signal = ~true_entry_signal
+    
+    true_entry_signal.name = 'entry_signal'
+    true_exit_signal.name = 'exit_signal'
+    max_combs.name = 'max_combs'
+    max_probs.name = 'max_probs'
 
-    return df_true_entry_signals, df_true_exit_signals # musia byt dfs
-
-# Create a custom indicator using the IndicatorFactory
-HigherHighStrategyML1 = vbt.IndicatorFactory(
-    input_names=['datasets', 'models', 'dataset_params'],
-    param_names=['probability_threshold'],
-    output_names = ['entry_signal', 'exit_signal']
-).from_apply_func(hhhl_ml1_strategy_logic)
+    return true_entry_signal, true_exit_signal, max_combs, max_probs
